4bd942b325767e54c82f0d8ef0deb008f4231e82
S-Moreira06 <moreira.s@outlook.com>
2025-04-18 09:59:15 +0200

    first commit

---
+++ resources/js/pages/home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="text-4xl font-bold">Bienvenue sur le blog test d'Inertia</h1>
+            <p className="mt-4 text-lg">Dans ce blog nous alons utiliser Inertia avec Laravel et React</p>
+        </div>
+    );
+}
